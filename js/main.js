6efa9912265f5d1320ca20cefa22a9a87b29c223
sukhrob5 <aktamovsuhrob577@gmail.com>
2022-12-16 18:10:11 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var firstName = prompt("ism")
+console.log(firstName)
+
+var lastName = prompt("familiya");
+console.log(lastName);
+
+var year = prompt("yil");
+console.log(year);
+
+var month = prompt("oy")
+console.log(month)
+
+var day = prompt("kun");
+console.log(day);
+
+var country = prompt("davlat");
+console.log(country);
+
+var city = prompt("shahar");
+console.log(city);
+
+var job = prompt("ish joyingiz")
+console.log(job)
+
+var phoneNumber = prompt("telefon raqamingiz");
+console.log(phoneNumber );
+
+var homeNumber = prompt("uy raqamingiz");
+console.log(homeNumber);
+
+
+// var about = "Yil" + year + "\n" + firstName
+
+var myAbout = "\n" + firstName + "\n" + lastName + "\n" + year + "\n" + month + "\n" + day + "\n" + country + "\n" + city + "\n" + job + "\n" + phoneNumber + "\n" + homeNumber;
+
+
+alert(myAbout);
+console.log(myAbout);
